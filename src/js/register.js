698baca1eea32490818a85f469545c30b6c1b61e
wcordstudio <pidque85@gmail.com>
2023-03-12 12:04:00 +0100

    JS fixed

--- src/js/register.js
+++ src/js/register.js
@@ -2,6 +2,6 @@ const price=0;
 var username;
 window.alert("Remember, this is for reservation only. When it is available, the reservation will be on the same page.")
 username=alert(prompt("Write your username:"))
-while(username!=null){
-    username=alert(prompt("Write your username:"))
-}
+// while(username!=null){
+//     username=alert(prompt("Write your username:"))
+// }
